c9806452051cd7e8c039ff64c1fc69c5a689b678
Shefin-Sir <95473621+Shefin-Sir@users.noreply.github.com>
2021-12-03 18:21:46 +0530

    Only for KellyMwol

--- helpers.js
+++ helpers.js
@@ -17,13 +17,13 @@
 */
 
 function successfullMessage(msg) {
-    return "✅ *Julie Mwol*:  ```" + msg + "```"
+    return "✅ *Kelly Mwol*:  ```" + msg + "```"
 }
 function errorMessage(msg) {
-    return "🛑 *Julie Mwol*:  ```" + msg + "```"
+    return "🛑 *Kelly Mwol*:  ```" + msg + "```"
 }
 function infoMessage(msg) {
-    return "⏺️ *Julie Mwol*:  ```" + msg + "```"
+    return "⏺️ *Kelly Mwol*:  ```" + msg + "```"
 }
 
 
